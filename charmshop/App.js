4b38a719caaaf3b644775a4954690ecba20a1458
H3LIOZZ <ishana.isler@gmail.com>
2023-11-26 00:09:01 +0100

    add forntend mockup

--- charmshop/App.js
+++ charmshop/App.js
@@ -1,18 +1,49 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View, TouchableWithoutFeedback, Linking, Image} from "react-native";
+import { NativeWindStyleSheet } from "nativewind";
+import { AntDesign } from "@expo/vector-icons";
+
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
 
 export default function App() {
+  const handleInstagramClick = () => {
+    Linking.openURL("https://www.instagram.com/");
+  };
+
   return (
-    <View className=" bg-gray-700 h-full">
-      <View className="flex items-center    mt-14">
-        <Text className="text-3xl text-center text-white">
+    <View className="flex-1 bg-gray-700 w-screen h-screen ">
+      <View className="flex-1 items-center">
+        <Text className="text-3xl text-white mt-20">
           Welcome to Charm Shop!
         </Text>
-        <Text className="text-white">
-          Open up App.js to start working on your app!
+        <Text className="text-xl text-slate-50 italic ">Made with Love</Text>
+        <Text className="text-3xl text-white mt-10">
+          Here are our Products!
         </Text>
-        <StatusBar style="auto" />
+        
+        <View className="bg-gray-800 w-96 h-96 absolute mt-64 rounded-xl ">
+          <Image
+           source={require('./IMG_5769.jpg')}
+           className="h-96 w-96 rounded-xl"
+          >
+
+          </Image>
+          <View className="bg-gray-900 p-5 mt-auto opacity-80 rounded-xl">
+            <Text className="text-white text-lg italic">Lorem ipsum dolor sit amet, </Text>
+          </View>
+        </View>
+        
+     <View  className="align-bottom mt-auto mb-20 flex flex-row items-center">
+        <TouchableWithoutFeedback onPress={handleInstagramClick} className=" flex ">
+          <AntDesign name="instagram" size={70} color="white" />
+        </TouchableWithoutFeedback>
+        <TouchableWithoutFeedback onPress={handleInstagramClick} className=" flex ">
+           <Text className="text-3xl text-white italic">charmshop</Text>
+        </TouchableWithoutFeedback>
       </View>
-    </View>
+  </View>
+      
+</View>
   );
 }
